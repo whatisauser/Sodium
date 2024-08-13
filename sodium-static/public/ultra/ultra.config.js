33696708dd1190f8ebd7f991837be53a60e5ac93
whatisausername <77344772+whatisauser@users.noreply.github.com>
2024-08-13 11:01:44 -0400

    Update ultra.config.js

--- sodium-static/public/ultra/ultra.config.js
+++ sodium-static/public/ultra/ultra.config.js
@@ -11,7 +11,7 @@ self.__uv$config = {
      * @type {string}
      */
     bare: [
-        "https://backend.infrared.bomberfish.ca"
+        "https://tomp.app/bare/"
         // Using a backend? Use this instead of Tomp.app:
         // "/bare1/",
         // "/bare2/",
